ec16f96b62fbe0088fbe6cee55589b5c31870220
Johannes Tegnér <johannes@jitesoft.com>
2019-11-25 21:23:10 +0100

    Typings added.

---
+++ group-by.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Groups each item in the list by a defined callback return value.
+ * Input is expected to be an array-like structure and return value will be a standard map/object.
+ *
+ * @param list     Array to group.
+ * @param callback Callback to execute on each object in the array to fetch key for grouping.
+ * @return Object in which keys are selected from passed list and the items are the items which are grouped under the key.
+ */
+export default function groupBy(list: any[], callback: (object) => void): object;
