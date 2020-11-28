5a4d5808f38c8b0c23333cae157668a6a98fcf3b
Johannes Tegnér <johannes@jitesoft.com>
2020-11-28 15:38:14 +0100

    Added 'groupByAsync method. Updated packages.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- src/index.js
+++ src/index.js
@@ -3,16 +3,42 @@
  * Input is expected to be an array-like structure and return value will be a standard map/object.
  *
  * @param {Array<*>} list     Array to group.
- * @param {Function} callback Callback to execute on each object in the array to fetch key for grouping.
+ * @param {Function} keySelector Callback to execute on each object in the array to fetch key for grouping.
  * @return {Object}
  */
-const groupBy = (list, callback) => {
+const groupBy = (list, keySelector) => {
   const result = {};
   const count = list.length;
   let val = null;
 
   for (let i = 0; i < count; i++) {
-    val = callback(list[i]);
+    val = keySelector(list[i]);
+
+    if (result[val] === undefined) {
+      result[val] = [];
+    }
+
+    result[val].push(list[i]);
+  }
+
+  return result;
+};
+
+/**
+ * Groups each item in the list by a defined callback return value.
+ * Input is expected to be an array-like structure and return value will be a standard map/object.
+ *
+ * @param {Array<*>} list     Array to group.
+ * @param {Function} keySelector Callback to execute on each object in the array to fetch key for grouping.
+ * @return {Promise<Object>}
+ */
+const groupByAsync = async (list, keySelector) => {
+  const result = {};
+  const count = list.length;
+  let val = null;
+
+  for (let i = 0; i < count; i++) {
+    val = await keySelector(list[i]);
 
     if (result[val] === undefined) {
       result[val] = [];
@@ -25,3 +51,8 @@ const groupBy = (list, callback) => {
 };
 
 export default groupBy;
+
+export {
+  groupBy,
+  groupByAsync
+};
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,4 @@
-import groupBy from '../src/index';
+import { groupBy, groupByAsync } from '../src/index';
 
 describe('Group objects under given key.', () => {
   test('Tests grouping.', () => {
@@ -11,6 +11,35 @@ describe('Group objects under given key.', () => {
       { id: 'def', value: 'def' },
       { id: 'ghi', value: 'abc' }
     ], (o) => o.id);
+
+    expect(result).toEqual({
+      abc: [
+        { id: 'abc', value: 'abc' },
+        { id: 'abc', value: 'def' },
+        { id: 'abc', value: 'ghi' }
+      ],
+      def: [
+        { id: 'def', value: 'abc' },
+        { id: 'def', value: 'def' }
+      ],
+      ghi: [
+        { id: 'ghi', value: 'abc' }
+      ]
+    });
+  });
+});
+
+describe('Group objects under given key async.', () => {
+  test('Tests grouping.', async () => {
+    const result = await groupByAsync([
+      { id: 'abc', value: 'abc' },
+      { id: 'abc', value: 'def' },
+      { id: 'abc', value: 'ghi' },
+
+      { id: 'def', value: 'abc' },
+      { id: 'def', value: 'def' },
+      { id: 'ghi', value: 'abc' }
+    ], (o) => Promise.resolve(o.id));
 
     expect(result).toEqual({
       abc: [
