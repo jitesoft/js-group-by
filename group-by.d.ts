518270669004030ffb6dc158f959c3482e7176e9
Johannes Tegnér <johannes@jitesoft.com>
2021-04-05 11:09:00 +0200

    Updated d.ts file with async method. Updated packages to latest versions.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- group-by.d.ts
+++ group-by.d.ts
@@ -4,6 +4,16 @@
  *
  * @param list     Array to group.
  * @param callback Callback to execute on each object in the array to fetch key for grouping.
- * @return Object in which keys are selected from passed list and the items are the items which are grouped under the key.
+ * @return Object  in which keys are selected from passed list and the items are the items which are grouped under the key.
  */
 export default function groupBy(list: any[], callback: (any) => void): object;
+
+/**
+ * Groups each item in the list by a defined callback return value.
+ * Input is expected to be an array-like structure and return value will be a standard map/object.
+ *
+ * @param list             Array to group.
+ * @param callback         Callback to execute on each object in the array to fetch key for grouping.
+ * @return Promise<Object> in which keys are selected from passed list and the items are the items which are grouped under the key.
+ */
+export function groupByAsync(list: any[], callback: (any) => Promise<void>): Promise<object>
